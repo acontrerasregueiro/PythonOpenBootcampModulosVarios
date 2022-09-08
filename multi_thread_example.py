3899769b1de4faadfa227cbe6ef15fd9c6bb2918
Adrian <acontrerasregueiro@gmail.com>
2022-09-08 06:34:55 +0200

    anadida buena practica main en multithread

--- multi_thread_example.py
+++ multi_thread_example.py
@@ -4,22 +4,22 @@ import _thread
 import time
 
 def main():
-    pass
+    def horaActual(nombre_thread, tiempo_de_espera):
+        
+        count = 0
+        
+        while count < 3:
+            time.sleep(tiempo_de_espera)
+            print(f'hilo : {nombre_thread} - {time.ctime(time.time())}')
+            count += 1
 
-def horaActual(nombre_thread, tiempo_de_espera):
-    
-    count = 0
-    
-    while count < 3:
-        time.sleep(tiempo_de_espera)
-        print(f'hilo : {nombre_thread} - {time.ctime(time.time())}')
-        count += 1
+    _thread.start_new_thread(horaActual,('Ejecutando thread uno',1)) 
+    _thread.start_new_thread(horaActual,('Ejecutando thread dos',4))
 
-_thread.start_new_thread(horaActual,('Ejecutando thread uno',1)) 
-_thread.start_new_thread(horaActual,('Ejecutando thread dos',4))
+    while True:
+        time.sleep(1)
+        
 
-while True:
-    time.sleep(1)
 
 if __name__ == "__main__":
     main()
